c333d56af031a805ad60e58570142abcfc856b27
Pablo Ogata <pabloogata@gmail.com>
2019-02-20 17:49:13 -0500

    new UI

---
+++ app/src/main/java/com/example/ntermediate_android_development_project_week_kickoff_part2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ntermediate_android_development_project_week_kickoff_part2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/ntermediate_android_development_project_week_kickoff_part2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.ntermediate_android_development_project_week_kickoff_part2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
